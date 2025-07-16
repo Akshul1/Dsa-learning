34cf2cfddd6eb343421d03bf7fb583b3a1875732
akshul <jamwalakshu2004l@gmail.com>
2025-07-17 00:55:31 +0530

    solved array medium questions

--- src/z_faltu.java
+++
@@ -1,9 +0,0 @@
-public class z_faltu
-{
-    public static void main(String[] args) {
-
-        for(int i=0;i<10;i++) {
-            System.out.println(" this program is written to add one comtribution in the github on 13 july 2025");
-        }
-    }
-}
