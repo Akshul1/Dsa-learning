898a933856407e3f995d2e3aca757ac39aaf9539
akshul <jamwalakshu2004l@gmail.com>
2025-07-20 22:28:47 +0530

    solved array medium questions

---
+++ src/e_Array/bm6_1.java
@@ -0,0 +1,11 @@
+package e_Array;
+// Another variety of bm6
+// There’s an array ‘A’ of size ‘N’ with positive and negative elements (not necessarily equal). Without altering the relative
+// order of positive and negative elements, you must return an array of alternately positive and negative values. The leftover
+// elements should be placed at the very end in the same order as in array A.
+public class bm6_1
+{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/e_Array/bm7.java
@@ -0,0 +1,45 @@
+package e_Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+//next_permutation
+public class bm7
+{
+    public static List<Integer> Brute(List<Integer>a){
+//        do this brute approach while revise;
+        int n=a.size(); // size of the array;
+
+//        step 1 find the breaking point
+        int ind=-1;  // stores the index of the breaking point
+        for(int i=n-2;i>=0;i--){// n-1 leads to index out of bound
+            if (a.get(i)<a.get(i+1)){
+                ind=i;
+                break;
+            }
+        }
+//        if breaking point doesnot exist
+        if (ind == -1) {
+            Collections.reverse(a);
+            return a;
+        }
+
+
+
+        return a;
+
+    }
+    public static void main(String[] args)
+    {
+        List<Integer> a= Arrays.asList(new Integer[]{2, 1, 5, 4, 3, 0, 0});
+        List<Integer> ans= Brute(a);
+
+        System.out.print("Next permutation is [");
+        for(int i=0;i<ans.size();i++){
+            System.out.println(ans.get(i)+ "");
+        }
+        System.out.println("]");
+    }
+}
