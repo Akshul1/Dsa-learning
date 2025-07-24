624049a1639108536bfff144bdf1eeeac77d9547
jamwa <jamwalakshu2004l@gmail.com>
2025-07-24 18:53:43 +0530

    edited some files

--- src/e_Array/easy/ae3.java
+++ src/e_Array/easy/ae3.java
@@ -1,3 +1,23 @@
 package e_Array.easy;
 
+import java.util.Arrays;
+
+public class ae3 {
+    public static boolean Optimal(int []nums){
+        int n=0;
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n])
+                count++;
+
+        }
+        return count <=1;
+    }
+    public static void main(String[] args) {
+        int []nums={3,4,5,1,2};
+
+        System.out.println("Input "+ Arrays.toString(nums));
+        System.out.println(Optimal(nums));
+    }
 }
